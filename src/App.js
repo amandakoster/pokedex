087be6674c0199ba57fffb254cc01f312a8e88f3
Amanda Koster <amandakoster@gmail.com>
2017-11-01 11:23:40 -0700

    working on api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
+import PokeList from './components/PokeList'
 import './App.css';
 
-// const API_URL = 'http://pokeapi.co/api/v2'
+
 
 
 //build hello world
-//make pokeman component that calls api
-//api call with limit to 12
-//print data as objects in console log
+//render compenents here
+
 
 class App extends Component {
   render(){
     return(
       <div>
         <h1> PokeDex! </h1>
+        <PokeList />
         </div>
     );
   }
