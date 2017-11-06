087fc06f6d8878adfc46c84d2ed2b24f72a4f10b
Amanda Koster <amandakoster@gmail.com>
2017-11-05 18:15:50 -0800

    more api calls to get more pokemon info in order to print to the DOM

---
+++ src/components/PokeList.js
@@ -0,0 +1,26 @@
+// import React, { Component } from 'react';
+
+
+
+//   const PokeGride = () => (
+//     <div style={styles.root}>
+//       <GridList
+//         cellHeight={180}
+//         style={styles.gridList}
+//       >
+//         <Subheader>December</Subheader>
+//         {tilesData.map((tile) => (
+//           <GridTile
+//             key={tile.img}
+//             title={tile.title}
+//             subtitle={<span>by <b>{tile.author}</b></span>}
+//             actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+//           >
+//             <img src={tile.img} />
+//           </GridTile>
+//         ))}
+//       </GridList>
+//     </div>
+//   );
+  
+//   export default PokeGrid;
