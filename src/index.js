03c266b6c7da3ac1e3095ef8bece4ae2af0b2fe1
Amanda Koster <amandakoster@gmail.com>
2017-11-11 18:02:15 -0800

    deployed to heroku cleaning up the files writing README

--- src/index.js
+++ src/index.js
@@ -3,18 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import * as firebase from 'firebase';
-
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAiPCSBoUiwTsMw1crilOE5PANkP9DHM_g",
-    authDomain: "pokedex-f9a50.firebaseapp.com",
-    databaseURL: "https://pokedex-f9a50.firebaseio.com",
-    projectId: "pokedex-f9a50",
-    storageBucket: "pokedex-f9a50.appspot.com",
-    messagingSenderId: "577463132239"
-  };
-  firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
