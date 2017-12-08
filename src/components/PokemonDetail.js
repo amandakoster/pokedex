fc6fab50edbf516be52641e4f3aaf483e98c9597
Amanda Koster <amandakoster@gmail.com>
2017-12-07 21:22:02 -0800

    map abilites and moves

---
+++ src/components/PokemonDetail.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+class PokemonDetail extends Comment {
+    constructor(props){
+        super(props)
+        this.getType = this.getType.bind(this)
+    }
+
+    getType() {
+        if (this.props.pokeman.name) {
+            return this.props.pokeman.types.reduce( (primaryType, type) => {
+                primaryType = type.slot == 1 ? type.type.nmae : '';
+                return primaryType;
+            }, '');
+
+        }
+    }
+
+    render() {
+        return(
+            this.props.pokeman.name ?
+            <div id="pokemonContainer" className={this.getType()}>
+                <h2>{this.props.pokemon.name}</h2>
+                    <div className="detailWrapper">
+                        <div>
+                            <img src={this.props.pokeman.sprits.front_shiny} />
+                        </div>
+
+                        <div>
+                            <h3>Abilites</h3>
+                                <ul>{this.props.pokemon.abilities.map((value, i) => 
+                                <li key={i}>{value.ability.name}</li>
+                                )}
+                                </ul>
+                        </div>
+
+                        <div>
+                            <h3>Moves</h3>
+                            <ul>{this.props.pokeman.moves.map((value, i) =>
+                            <li key={i}>{value.move.name}</li>
+                            )}
+                            </ul>
+                        </div>
+                            
+                    </div>
+            </div>
+
+            : <div id="pokemonContainer"></div>
+        )
+    }
+}
+
+expot default PokemonDetail
